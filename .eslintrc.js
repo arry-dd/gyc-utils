991986093b3f76d40be7c43ca3f50176819c9905
gyc <guanyanchang@52tt.com>
2022-10-17 18:26:42 +0800

    feat: 新增prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,24 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ['eslint:recommended', 'standard-with-typescript'],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: 'tsconfig.json',
-  },
-  rules: {
-    'no-param-reassign': 'error',
-    'comma-dangle': ['error', 'only-multiline'],
-    'semi-style': ['error', 'last'],
-  },
-}
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'standard-with-typescript',
+		'plugin:prettier/recommended',
+		'prettier',
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: 'tsconfig.json',
+	},
+	rules: {
+		'no-param-reassign': 'error',
+		'comma-dangle': ['error', 'only-multiline'],
+		'semi-style': ['error', 'last'],
+	},
+};
