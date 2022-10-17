5172f2f31bff2683c9006d11498cf58b85aeb767
gyc <guanyanchang@52tt.com>
2022-10-17 15:33:35 +0800

    feat: 初始化项目

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,25 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import pkg from './package.json';
+// import resolve from '@rollup/plugin-node-resolve';
+// import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import fs from 'fs'
+const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
 export default [
-	// browser-friendly UMD build
 	{
-		input: 'src/main.js',
+		input: './index.ts',
 		output: {
 			name: 'howLongUntilLunch',
 			file: pkg.browser,
 			format: 'umd'
 		},
 		plugins: [
-			resolve(), // so Rollup can find `ms`
-			commonjs() // so Rollup can convert `ms` to an ES module
+			// resolve(), // 保证绝对路径的正常使用
+			// commonjs() // 处理没有导出声明的库
+			typescript() // 提供typescript的支持
 		]
 	},
-
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify
-	// `file` and `format` for each target)
 	{
-		input: 'src/main.js',
+		input: './index.ts',
 		external: ['ms'],
 		output: [
 			{ file: pkg.main, format: 'cjs' },
