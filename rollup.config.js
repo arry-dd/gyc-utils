2cf973234a6e812a2326908f35ea62e157d8146c
gyc <guanyanchang@52tt.com>
2022-10-17 16:19:57 +0800

    feat: 修改ts的编译问题

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,22 @@
 // import resolve from '@rollup/plugin-node-resolve';
 // import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import { cleandir } from "rollup-plugin-cleandir";
 import fs from 'fs'
 const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
-export default [
-	{
-		input: './index.ts',
-		output: {
-			name: 'howLongUntilLunch',
-			file: pkg.browser,
-			format: 'umd'
-		},
-		plugins: [
-			// resolve(), // 保证绝对路径的正常使用
-			// commonjs() // 处理没有导出声明的库
-			typescript() // 提供typescript的支持
-		]
-	},
-	{
-		input: './index.ts',
-		external: ['ms'],
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
-		]
-	}
-];
+
+export default {
+	input: './src/index.ts',
+	output: [
+		{ file: pkg.main, format: 'cjs', sourcemap: true, },
+		{ name: 'index', file: pkg.browser, format: 'umd', sourcemap: true, },
+		{ file: pkg.module, format: 'es', sourcemap: true, }
+	],
+	plugins: [
+		cleandir('./dist'),
+		// resolve(), // 保证绝对路径的正常使用
+		// commonjs(), // 处理没有导出声明的库
+		typescript() // 提供typescript的支持
+	]
+}
