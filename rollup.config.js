28dedcb0b90bc351b95baddf46242de20faa67a7
gyc <guanyanchang@52tt.com>
2022-10-17 18:42:27 +0800

    build: 新增git提交检查

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,23 @@
 // import resolve from '@rollup/plugin-node-resolve';
 // import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import { cleandir } from "rollup-plugin-cleandir";
+import { cleandir } from 'rollup-plugin-cleandir';
 import eslint from '@rollup/plugin-eslint';
-import fs from 'fs'
+import fs from 'fs';
 const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-
 
 export default {
 	input: './src/index.ts',
 	output: [
-		{ file: pkg.main, format: 'cjs', sourcemap: true, },
-		{ name: 'index', file: pkg.browser, format: 'umd', sourcemap: true, },
-		{ file: pkg.module, format: 'es', sourcemap: true, }
+		{ file: pkg.main, format: 'cjs', sourcemap: true },
+		{ name: 'index', file: pkg.browser, format: 'umd', sourcemap: true },
+		{ file: pkg.module, format: 'es', sourcemap: true },
 	],
 	plugins: [
 		cleandir('./dist'),
 		// resolve(), // 保证绝对路径的正常使用
 		// commonjs(), // 处理没有导出声明的库
 		eslint(),
-		typescript() // 提供typescript的支持
-	]
-}
+		typescript(), // 提供typescript的支持
+	],
+};
--- test/test.js
+++ test/test.js
@@ -1,22 +1,34 @@
 const assert = require('assert');
 const howLongTillLunch = require('..');
 
-function MockDate () {
+function MockDate() {
 	this.date = 0;
 	this.hours = 0;
 	this.minutes = 0;
 	this.seconds = 0;
 	this.milliseconds = 0;
-};
+}
 
 Object.assign(MockDate.prototype, {
-	getDate () { return this.date; },
-	setDate (date) { this.date = date; },
-	setHours (h) { this.hours = h; },
-	setMinutes (m) { this.minutes = m; },
-	setSeconds (s) { this.seconds = s; },
-	setMilliseconds (ms) { this.milliseconds = ms; },
-	valueOf () {
+	getDate() {
+		return this.date;
+	},
+	setDate(date) {
+		this.date = date;
+	},
+	setHours(h) {
+		this.hours = h;
+	},
+	setMinutes(m) {
+		this.minutes = m;
+	},
+	setSeconds(s) {
+		this.seconds = s;
+	},
+	setMilliseconds(ms) {
+		this.milliseconds = ms;
+	},
+	valueOf() {
 		return (
 			this.milliseconds +
 			this.seconds * 1e3 +
@@ -24,7 +36,7 @@ Object.assign(MockDate.prototype, {
 			this.hours * 1e3 * 60 * 60 +
 			this.date * 1e3 * 60 * 60 * 24
 		);
-	}
+	},
 });
 
 const now = new MockDate();
@@ -41,7 +53,7 @@ function test(hours, minutes, seconds, expected) {
 	console.log(`\u001B[32m✓\u001B[39m ${expected}`);
 }
 
-let lunchtime = [ 12, 30 ];
+let lunchtime = [12, 30];
 test(11, 30, 0, '1 hour');
 test(10, 30, 0, '2 hours');
 test(12, 25, 0, '5 minutes');
@@ -49,5 +61,5 @@ test(12, 29, 15, '45 seconds');
 test(13, 30, 0, '23 hours');
 
 // some of us like an early lunch
-lunchtime = [ 11, 0 ];
+lunchtime = [11, 0];
 test(10, 30, 0, '30 minutes');
